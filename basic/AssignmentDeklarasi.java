9017b0cc48b4f00e2c3e210f33972f45e80aab6a
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-13 19:48:13 +0700

    Assignment dan Deklarasi

---
+++ basic/AssignmentDeklarasi.java
@@ -0,0 +1,16 @@
+public class AssignmentDeklarasi {
+
+    public static void main(String[] args) {
+        int a = 10; // bentuk dari assignment
+        // mendeklarasikan variabel a sebagai tipe int, tapi juga langsung memberikan nilai awal padanya.
+        System.out.println("nilai a " + a);
+        
+        a = 20;
+        System.out.println("nilai a yang baru " + a);
+
+        int b; // bentuk dari deklarasi
+        // memberitahu kompiler bahwa kita memiliki variabel yang bernama b, yang akan digunakan untuk menyimpan nilai-nilai yang bertipe int
+        b = 8;
+        System.out.println(b);
+    }
+}
