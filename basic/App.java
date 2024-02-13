c2db860ebeef728ca163e7b9bc393b03360cc501
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-13 19:41:12 +0700

    Struktur Penulisan Program

---
+++ basic/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) { //metode utama yang dijalankan ketika program Java dijalankan
+        // String[] => mendeklarasikan sebuah array yang dapat menyimpan sejumlah elemen bertipe String
+        // [] => merupakan array
+        // args =>  Array args digunakan untuk menangkap command-line arguments
+        System.out.println("Hello World");
+    }
+}
