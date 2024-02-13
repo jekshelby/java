045e423ee366ffaaf5605064c5bdacfd7d1ac547
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-13 21:38:45 +0700

    Operator Aritmatika

---
+++ basic/Operator/OperatorAritmatika.java
@@ -0,0 +1,30 @@
+package Operator;
+
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Penjumlahan
+        int hasilTambah = a + b;
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+
+        // Pengurangan
+        int hasilKurang = a - b;
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+
+        // Perkalian
+        int hasilKali = a * b;
+        System.out.println("Hasil Perkalian: " + hasilKali);
+
+        // Pembagian
+        float c = 10;
+        float d = 25;
+        float hasilBagi = c / d;
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+
+        // modulus
+        int hasilModulus = a % b;
+        System.out.println("Hasil Modulus: " + hasilModulus);
+    }
+}
