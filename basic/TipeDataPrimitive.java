a0815192c6a5a2343c840810ffe6cb199b5926af
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-13 21:19:36 +0700

    Tipe Data

---
+++ basic/TipeDataPrimitive.java
@@ -0,0 +1,19 @@
+public class TipeDataPrimitive {
+    public static void main(String[] args) {
+        // Deklarasi dan assignment tipe data integer
+        int jumlahProduk = 50;
+        System.out.println("Jumlah Produk: " + jumlahProduk);
+
+        // Deklarasi dan assignment tipe data floating point
+        double hargaProduk = 99.99;
+        System.out.println("Harga Produk: " + hargaProduk);
+
+        // Deklarasi dan assignment tipe data boolean
+        boolean stokTersedia = true;
+        System.out.println("Stok Tersedia: " + stokTersedia);
+
+        // Deklarasi dan assignment tipe data karakter
+        char kategoriProduk = 'A';
+        System.out.println("Kategori Produk: " + kategoriProduk);
+    }
+}
