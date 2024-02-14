cc44b96c2f4316b4e692dd8d9ab8ed9bac9cd0df
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-14 11:26:21 +0700

    Operator Perbandingan

---
+++ basic/Operator/OperatorPerbandingan.java
@@ -0,0 +1,17 @@
+package Operator;
+
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        // ==, !=, >, <, >=, <=
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b)); // false
+        System.out.println("a < b: " + (a < b)); // true
+        System.out.println("a >= b: " + (a >= b)); // false
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
